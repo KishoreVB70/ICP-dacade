7251f820f28d91314b5ff1be6c98cc53978b45e2
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-16 11:45:39 +0530

    added the address of the user into the struct

--- src/icp_rust_boilerplate_backend/src/oldeson.rs
+++ src/icp_rust_boilerplate_backend/src/oldeson.rs
@@ -1,19 +1,25 @@
 #[macro_use]
+// Rust data structures
 extern crate serde;
+
+// Interafce of canister
 use candid::{Decode, Encode};
+// Canister development kit -> interact with the ICP network
 use ic_cdk::api::time;
-use ic_cdk::api;
-use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+// Stable data structures across updateds
 use ic_stable_structures::{BoundedStorable, Cell, DefaultMemoryImpl, StableBTreeMap, Storable};
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+// Standard library of rust
 use std::{borrow::Cow, cell::RefCell};
 
+// Store state of canister
 type Memory = VirtualMemory<DefaultMemoryImpl>;
+// Generatge unique id
 type IdCell = Cell<u64, Memory>;
 
 #[derive(candid::CandidType, Clone, Serialize, Deserialize, Default)]
 struct Message {
     id: u64,
-    creator_address: String,
     title: String,
     body: String,
     attachment_url: String,
@@ -21,8 +27,12 @@ struct Message {
     updated_at: Option<u64>,
 }
 
-// a trait that must be implemented for a struct that is stored in a stable struct
+// Convert message(struct) into byte and byte into message
+// impl is the keyword used as implementation of a trait
+// Trait is like an interface
 impl Storable for Message {
+
+    // Self means the type that the function is present inside -> most likely Message
     fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
         Cow::Owned(Encode!(self).unwrap())
     }
@@ -32,28 +42,35 @@ impl Storable for Message {
     }
 }
 
-// another trait that must be implemented for a struct that is stored in a stable struct
+// maximum length of the struct and whether it is fixed or not
 impl BoundedStorable for Message {
     const MAX_SIZE: u32 = 1024;
     const IS_FIXED_SIZE: bool = false;
 }
 
+// Store the canister state which can be accessed from any part of the code
+// All these are just variables which can be called from any thread
 thread_local! {
+
+    // Store canisters virtual memory so that it can be accessed from anywhere in the code
     static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> = RefCell::new(
         MemoryManager::init(DefaultMemoryImpl::default())
     );
 
+    // ID  count of the canister
     static ID_COUNTER: RefCell<IdCell> = RefCell::new(
         IdCell::init(MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))), 0)
             .expect("Cannot create a counter")
     );
 
+    // Canister's storage
     static STORAGE: RefCell<StableBTreeMap<u64, Message, Memory>> =
         RefCell::new(StableBTreeMap::init(
             MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1)))
     ));
 }
 
+// Input from the user for a new entry
 #[derive(candid::CandidType, Serialize, Deserialize, Default)]
 struct MessagePayload {
     title: String,
@@ -61,29 +78,47 @@ struct MessagePayload {
     attachment_url: String,
 }
 
+// Custom error which we use when the data is not found
+#[derive(candid::CandidType, Deserialize, Serialize)]
+enum Error {
+    NotFound { msg: String },
+}
+
+
+
+// Obtain message from canister
+// First line indicates that this method can be called from the outside as a query
 #[ic_cdk::query]
 fn get_message(id: u64) -> Result<Message, Error> {
+    // match is similar to switch case
     match _get_message(&id) {
+        //Returns the message
         Some(message) => Ok(message),
         None => Err(Error::NotFound {
-            msg: format!("a message with id={} not found", id),
+            msg: format!("Message with id={} not found", id),
         }),
     }
 }
 
+// Actual function which retreives the message from the thread storage
+// s is the reference of the canister storage
+fn _get_message(id: &u64) -> Option<Message> {
+    STORAGE.with(|s| s.borrow().get(id))
+}
+
+// Returns whether the message was inserted or not -> could have used Result
 #[ic_cdk::update]
 fn add_message(message: MessagePayload) -> Option<Message> {
+
+    // obtain the counter value and increment it
     let id = ID_COUNTER
         .with(|counter| {
             let current_value = *counter.borrow().get();
             counter.borrow_mut().set(current_value + 1)
         })
         .expect("cannot increment id counter");
-
-    let puffy: String = api::caller().to_string();
     let message = Message {
         id,
-        creator_address: puffy,
         title: message.title,
         body: message.body,
         attachment_url: message.attachment_url,
@@ -92,6 +127,11 @@ fn add_message(message: MessagePayload) -> Option<Message> {
     };
     do_insert(&message);
     Some(message)
+}
+
+// Performs the actual insert into the storage
+fn do_insert(message: &Message) {
+    STORAGE.with(|service| service.borrow_mut().insert(message.id, message.clone()));
 }
 
 #[ic_cdk::update]
@@ -114,11 +154,6 @@ fn update_message(id: u64, payload: MessagePayload) -> Result<Message, Error> {
     }
 }
 
-// helper method to perform insert.
-fn do_insert(message: &Message) {
-    STORAGE.with(|service| service.borrow_mut().insert(message.id, message.clone()));
-}
-
 #[ic_cdk::update]
 fn delete_message(id: u64) -> Result<Message, Error> {
     match STORAGE.with(|service| service.borrow_mut().remove(&id)) {
@@ -130,16 +165,6 @@ fn delete_message(id: u64) -> Result<Message, Error> {
             ),
         }),
     }
-}
-
-#[derive(candid::CandidType, Deserialize, Serialize)]
-enum Error {
-    NotFound { msg: String },
-}
-
-// a helper method to get a message by id. used in get_message/update_message
-fn _get_message(id: &u64) -> Option<Message> {
-    STORAGE.with(|service| service.borrow().get(id))
 }
 
 // need this to generate candid
